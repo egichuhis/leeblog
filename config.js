88203cbc6cf891d361be03e13600ae68a9077b0a
Edwin Gichuhi <edwingichuhis@gmail.com>
2020-08-13 19:51:41 +0300

    name update

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://leeccentric.netlify.app/',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'Blog by Lillian Wangechi',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
@@ -25,7 +25,7 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Lillian Wangechi',
     photo: '/photo.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
